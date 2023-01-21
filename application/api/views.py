ccc77cb696f4e028db13ad351355836322058908
AKafer <akafer@mail.ru>
2023-01-21 17:26:04 +0200

    add parser for new test, dockerfile

--- application/api/views.py
+++ application/api/views.py
@@ -1,6 +1,11 @@
 from django_filters import rest_framework as dfilters
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+import requests
+from bs4 import BeautifulSoup
+from rest_framework.response import Response
+import random
 
 from testing.models import Group, Test, Question, Answer, Result
 
@@ -29,6 +34,52 @@ class QuestionViewSet(viewsets.ModelViewSet):
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ('test', )
     
+    @action(
+        methods=['get'],
+        detail=False,
+        url_path=r'new_test')
+    def get_data(self, request):
+        print('***test***')
+        url = 'https://centrevraz.ru/dlya-rvp-vnzh'
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'lxml')
+        questions = soup.find_all('div', {'class' : 'test_t'})
+        new_group = Group.objects.get_or_create(name='новая группа')
+        new_test = Test.objects.get_or_create(name='новый тест', group=new_group[0])
+        answers = Answer.objects.all()
+        for answer in answers:
+            if answer.id > 46:
+                answer.delete()
+        i = 0
+        for question in questions:
+            i += 1
+            q = str(question.find('b')).strip('<b></b>')
+            new_q = Question.objects.get_or_create(
+                question = q,
+                number = i,
+                test = new_test[0]
+            )
+            answers = question.find_all('span')
+            right_ans = random.randint(1, len(answers))
+            j = 0
+            for ans in answers:
+                j += 1
+                ans_to_bd = str(ans).strip('<span></span>')
+                correct = False
+                if j == right_ans:
+                    correct = True
+                new_ans = Answer.objects.get_or_create(
+                    name = ans_to_bd,
+                    question = new_q[0],
+                    correct = correct
+                )
+        new_ques = Question.objects.filter(test=new_test[0])
+        serializer = QuestionSerializer(new_ques, many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+        
+        
+        
+        
 class AnswerViewSet(viewsets.ModelViewSet):
     """Класс представления клиентов"""
     queryset = Answer.objects.all()
--- application/static/js/script.js
+++ application/static/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   let sel_test = $("#select_test");
   let set_questions = new Map();
   let set_answers = new Map();
+  let set_ans = new Map();
   let number_ques = 0;
   let number_suq = 0;
 
@@ -112,10 +113,10 @@ $(document).ready(function () {
     } else {
       alert("Тест окончен. Смотрите результаты в таблице.");
       $("#TestModal").fadeOut();
-      console.log("РЕЗУЛЬТАТЫ");
-      console.log(number_ques);
-      console.log(number_suq);
-      console.log(Math.round((number_suq / number_ques) * 100));
+      // console.log("РЕЗУЛЬТАТЫ");
+      // console.log(number_ques);
+      // console.log(number_suq);
+      // console.log(Math.round((number_suq / number_ques) * 100));
       $("#input_number_questions").val(number_ques);
       $("#input_success_questions").val(number_suq);
       $("#input_persent").val(
@@ -128,14 +129,16 @@ $(document).ready(function () {
     $("#answer_container").empty();
     $("#input_text").val("");
     set_answers.clear();
+    set_ans.clear();
     $.ajax({
       type: "GET",
       url: "api/answers/?question=" + question_id,
       success: function (response) {
         for (i = 0; i < response.length; i++) {
           set_answers.set(response[i].id, response[i].correct);
+          set_ans.set(response[i].id, [response[i].name, response[i].correct]);
           $("#answer_container").append(
-            `<label><input type="checkbox" id="ch${response[i].id}" />&#160&#160${response[i].name}</label><p></p>`
+            `<label id="lbl${response[i].id}"><input type="checkbox" id="ch${response[i].id}" />&#160&#160${response[i].name}</label><p></p>`
           );
         }
       },
@@ -143,7 +146,6 @@ $(document).ready(function () {
   }
 
   function check_answer() {
-    console.log("ЧЕКБОКСЫ");
     for (let entry of set_answers) {
       console.log($(`#ch${entry}`).is(":checked"), entry[1]);
       if ($(`#ch${entry[0]}`).is(":checked") != entry[1]) {
@@ -157,5 +159,35 @@ $(document).ready(function () {
     console.log("NEXT");
     check_answer();
     next_question();
+  });
+
+  $("#RightAnswerButton").on("click", function () {
+    console.log(set_ans);
+    for (let entry of set_ans) {
+      const ch = document.querySelector(`#lbl${entry[0]}`);
+      const parent = ch.parentNode;
+      parent.removeChild(ch);
+    }
+    for (let entry of set_ans) {
+      if (entry[1][1] == true) {
+        $("#answer_container").append(
+          `<label id="lbl${entry[0]}"><input type="checkbox" checked="true" id="ch${entry[0]}" />&#160&#160${entry[1][0]}</label><p></p>`
+        );
+      } else {
+        $("#answer_container").append(
+          `<label id="lbl${entry[0]}"><input type="checkbox" id="ch${entry[0]}" />&#160&#160${entry[1][0]}</label><p></p>`
+        );
+      }
+    }
+  });
+
+  $("#NewTestButton").on("click", function () {
+    $.ajax({
+      type: "GET",
+      url: "api/questions/new_test/",
+      success: function (response) {
+        console.log(response);
+      },
+    });
   });
 });
